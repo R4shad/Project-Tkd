960da36bcd3bf92fa1208711dafb7293bab51d47
BeimarArispe <201900388@est.umss.edu>
2025-03-22 21:53:25 -0400

    HU-02/feat: Create navbar component and install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
 import './App.css'
+import { Home } from './components/Home'
+import Navbar from './components/Navbar'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom' 
 
 function App() {
   return (
     <>
-      <h1>Project-Tkd</h1>
+      
+      <Router>
+      <Navbar/>
+        {/* <nav className="navbar">
+          <p className="nav-title">Prueba Técnica</p>
+          <div className="nav-links">
+            <Link className="nav-link" to="/">
+              Lista de Tareas
+            </Link>
+            <Link className="nav-link" to="/create">
+              Crear Tarea
+            </Link>
+          </div>
+        </nav> */}
+
+        <div className="container">
+          <Routes>
+          <Route
+              path="/"
+              element={<Home/>}
+            />
+            {/* <Route
+              path="/"
+              element={<Home tasks={tasks} deleteTask={deleteTask} />}
+            />
+            <Route
+              path="/create"
+              element={<TaskForm createTask={createTask} editTask={editTask} />}
+            />
+            <Route
+              path="/edit/:id"
+              element={<TaskForm createTask={createTask} editTask={editTask} />}
+            /> */}
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,129 @@
+import { useState } from 'react';
+import logo from '../assets/logo.jpg';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          {/* Logo */}
+          <div className="flex-shrink-0 flex items-center">
+            <img
+              className="h-8 w-auto"
+              src={logo}
+              alt="Logo"
+            />
+          </div>
+
+          <div className="flex items-center md:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              onClick={toggleMenu}
+            >
+              <span className="sr-only">Abrir menú</span>
+            
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+
+ 
+          <div className="hidden md:flex md:items-center md:space-x-8">
+            <Link to={'/'} className="text-gray-900 hover:text-custom-primary-color">
+              Clubes
+            </Link>
+            <Link to={'/'} className="text-gray-900 hover:text-custom-primary-color">
+              Posts
+            </Link>
+            <Link to={'/'} className="text-gray-900 hover:text-custom-primary-color">
+            Acerca de
+           </Link>
+              
+          </div>
+        </div>
+      </div>
+
+
+      <div
+        className={`fixed inset-0 z-50 transform ${
+          isMenuOpen ? 'translate-x-0' : 'translate-x-full'
+        } transition-transform duration-300 ease-in-out`}
+      >
+       
+
+
+        <div className="fixed top-0 right-0 h-full w-3/4 bg-white shadow-lg">
+          <div className="flex justify-end p-4">
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-700 focus:outline-none"
+              onClick={toggleMenu}
+            >
+              <span className="sr-only">Cerrar menú</span>
+
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+
+
+          <div className="px-4 pt-2 pb-3 space-y-4">
+            <a
+              href="#"
+              className="block text-gray-900 hover:text-custom-primary-color"
+            >
+              Clubes
+            </a>
+            <a
+              href="#"
+              className="block text-gray-900 hover:text-custom-primary-color"
+            >
+              Posts
+            </a>
+            <a
+              href="#"
+              className="block text-gray-900 hover:text-custom-primary-color"
+            >
+              Acerca de
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
