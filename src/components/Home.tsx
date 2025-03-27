754849dc17ecf14d393d1340447620595793d50b
BeimarArispe <201900388@est.umss.edu>
2025-03-26 23:43:19 -0400

    HU-07/feat: Create Description component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import heroImg from '../assets/hero-img.png'
 import Teams from './Teams'
 import { ClubSlider } from './ClubSlider'
 import { TaekwondoBenefits } from './TaekwondoBenefits'
+import Description from './Description'
 
 export const Home = () => {
   return (
@@ -12,6 +13,8 @@ export const Home = () => {
            title={'Asociación Municipal de TaeKwonDo Quillacollo'} 
            description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.'} 
         />
+        {/* <Description /> */}
+        <Description />
         {/* <Teams /> */}
         <Teams />
       <ClubSlider />
