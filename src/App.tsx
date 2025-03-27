a34d924335bde71e840ef61d329c32f6dbe75687
BeimarArispe <201900388@est.umss.edu>
2025-03-26 22:50:47 -0400

    HU-03/fix: Fix styles in the Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css'
 import { Home } from './components/Home'
-import { Hero } from './components/Hero'
 import Navbar from './components/Navbar'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import heroImg from './assets/hero-img.png'
-import Teams from './components/Teams'
+
 
 function App() {
   return (
@@ -12,28 +10,7 @@ function App() {
 
       <Router>
         <Navbar />
-        <Hero 
-           image={heroImg} 
-           title={'Asociación Municipal de TaeKwonDo Quillacollo'} 
-           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.'} 
-        />
-        {/* <Teams /> */}
-        <Teams />
-       
-
-
-        {/* <nav className="navbar">
-          <p className="nav-title">Prueba Técnica</p>
-          <div className="nav-links">
-            <Link className="nav-link" to="/">
-              Lista de Tareas
-            </Link>
-            <Link className="nav-link" to="/create">
-              Crear Tarea
-            </Link>
-          </div>
-        </nav> */}
-
+        
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,19 @@
+import { Hero } from './Hero'
+import heroImg from '../assets/hero-img.png'
+import Teams from './Teams'
 import { ClubSlider } from './ClubSlider'
 import { TaekwondoBenefits } from './TaekwondoBenefits'
 
 export const Home = () => {
   return (
     <>
+       <Hero 
+           image={heroImg} 
+           title={'Asociación Municipal de TaeKwonDo Quillacollo'} 
+           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.'} 
+        />
+        {/* <Teams /> */}
+        <Teams />
       <ClubSlider />
       <TaekwondoBenefits />
     </>
