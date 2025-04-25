69fd216273bb6c9bdd7c865f1abfd5fe374c900f
Amir Barra <barra.amir14@gmail.com>
2025-04-24 21:21:31 -0400

    main/fix: Fix data

--- src/staticData.ts
+++ src/staticData.ts
@@ -12,7 +12,14 @@ export const antaraKwanInfo: ClubInfo = {
   facebookLink: 'https://www.facebook.com/asoc.tkd.qllo?locale=es_LA',
   whatsappContact:
     'https://api.whatsapp.com/send/?phone=60794773&text&type=phone_number&app_absent=0',
-  images: [],
+  images: [
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ant7.jpg',
+
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ant5.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ant4.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ant6.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ant3.jpg',
+  ],
   googleMapsLocation: [-17.38980037698756, -66.28119982208774],
   instructorHistory:
     'Soy cinturón negro 2do dan, múltiples veces campeón departamental y subcampeón nacional en diferentes eventos. Practico Taekwondo desde los 14 años y, como parte de mi proyecto de graduación universitaria, desarrollé una aplicación web para administrar campeonatos de Taekwondo, ayudando a muchos practicantes a mejorar su participación en competencias y su desarrollo deportivo.',
@@ -44,45 +51,50 @@ export const lincesKwanInfo: ClubInfo = {
 
 export const relampagosKwanInfo: ClubInfo = {
   id: 3,
-  clubName: 'Academia de Taekwondo Relampagos Kwan',
-  foundationDate: 'January 31, 2022',
+  clubName: 'Academia de Taekwondo Relámpagos Kwan ',
+  foundationDate: '28 de Septiembre, 2017',
   headInstructorName: 'Erick Trujillo',
   description:
-    'Antara Kwan es un club nuevo que nació con el propósito de formar, entrenar y cuidar la salud de las personas a través del hermoso arte del Taekwondo. Nuestro enfoque está en el desarrollo integral de cada practicante, promoviendo disciplina, respeto y bienestar físico. Buscamos construir una comunidad apasionada por este deporte, fomentando valores fundamentales en cada sesión de entrenamiento.',
+    'Relámpagos Kwan es un club de Taekwondo que busca desarrollar autodisciplina, técnica y confianza en sus miembros. Con instructores experimentados y un ambiente de apoyo, ofrecen clases para todos los niveles. Su objetivo es fomentar el crecimiento personal y la excelencia en el arte del Taekwondo, promoviendo valores como el respeto y la perseverancia. Su misión es proporcionar un entorno de aprendizaje seguro y motivador para que los miembros alcancen su máximo potencial. Ofrecen clases diarias y fines de semana para adaptarse a los horarios de cada persona, guiados por valores de respeto, disciplina y trabajo en equipo.',
   logoUrl:
-    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/r1.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/relogo.jpg',
   facebookLink:
     'https://www.facebook.com/profile.php?id=100063723433629&locale=es_LA',
   whatsappContact:
     'https://api.whatsapp.com/send/?phone=65725219&text&type=phone_number&app_absent=0',
-  images: [],
+  images: [
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ar1.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ar2.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ar4.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ar3.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ar5.jpg',
+  ],
   googleMapsLocation: [-17.39309902419564, -66.2619409338207],
   instructorHistory:
-    'Soy cinturón negro 2do dan, múltiples veces campeón departamental y subcampeón nacional en diferentes eventos. Practico Taekwondo desde los 14 años y, como parte de mi proyecto de graduación universitaria, desarrollé una aplicación web para administrar campeonatos de Taekwondo, ayudando a muchos practicantes a mejorar su participación en competencias y su desarrollo deportivo.',
+    'Erick Trujillo es un destacado atleta e instructor de Taekwondo con 22 años de experiencia en la disciplina, especializado en Kiorugui (combate). A lo largo de su carrera, ha participado en numerosas competiciones nacionales e internacionales, logrando múltiples Medallas de Oro, Plata y Bronce. Con más de 8 años como instructor, Erick ha enseñado a estudiantes de todas las edades y niveles, enfocándose en transmitir valores fundamentales como la disciplina, el respeto y la perseverancia. Su compromiso con la excelencia y la formación integral de sus alumnos ha llevado a varios de ellos a convertirse en campeones nacionales. Su enfoque centrado en el desarrollo tanto físico como mental lo convierte en un instructor excepcionalmente dedicado y respetado en el mundo del Taekwondo.',
 }
 
 export const aguilasKwanInfo: ClubInfo = {
   id: 4,
   clubName: 'Academia de Taekwondo "Águilas Kwan"',
-  foundationDate: 'January 31, 2022',
+  foundationDate: '24 de julio del 1999',
   headInstructorName: 'Henry Mercado',
   description:
-    'Antara Kwan es un club nuevo que nació con el propósito de formar, entrenar y cuidar la salud de las personas a través del hermoso arte del Taekwondo. Nuestro enfoque está en el desarrollo integral de cada practicante, promoviendo disciplina, respeto y bienestar físico. Buscamos construir una comunidad apasionada por este deporte, fomentando valores fundamentales en cada sesión de entrenamiento.',
+    'El Club ÁGUILAS se encuentra afiliado a la F.B.TKD, W.T.F forma deportistas con una educación integral, ética , así también alumnos campeones departamentales, nacionales',
   logoUrl:
     'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/a1_esuuq9.jpg',
   facebookLink: 'https://www.facebook.com/aguilaskwan?locale=es_LA',
   whatsappContact:
     'https://api.whatsapp.com/send/?phone=70351493&text&type=phone_number&app_absent=0',
   images: [
-    'https://res.cloudinary.com/ddkjviwgt/image/upload/v1745286449/l5_hydl74.jpg',
-    'https://res.cloudinary.com/ddkjviwgt/image/upload/v1745286450/l3_cosxkd.jpg',
-    'https://res.cloudinary.com/ddkjviwgt/image/upload/v1745286450/l1_mzo1nn.jpg',
-    'https://res.cloudinary.com/ddkjviwgt/image/upload/v1745286450/l2_no7sjw.jpg',
-    'https://res.cloudinary.com/ddkjviwgt/image/upload/v1745286450/l4_ucyyms.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ag1.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ag2.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ag3.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ag4.jpg',
+    'https://wwgxztrtjxckhgasxbrf.supabase.co/storage/v1/object/public/tkdqllo/StaticData/ag5.jpg',
   ],
-  googleMapsLocation: [-17.38980037698756, -66.28119982208774],
-  instructorHistory:
-    'Soy cinturón negro 2do dan, múltiples veces campeón departamental y subcampeón nacional en diferentes eventos. Practico Taekwondo desde los 14 años y, como parte de mi proyecto de graduación universitaria, desarrollé una aplicación web para administrar campeonatos de Taekwondo, ayudando a muchos practicantes a mejorar su participación en competencias y su desarrollo deportivo.',
+  googleMapsLocation: [-17.394573169796562, -66.28518529258521],
+  instructorHistory: '',
 }
 
 export const clubs: ClubInfo[] = [
