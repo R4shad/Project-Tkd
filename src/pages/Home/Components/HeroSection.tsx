df52713819a565d0269bf1ccdbdd8e51a548ddf5
Amir Barra <barra.amir14@gmail.com>
2025-05-25 17:26:48 -0400

    HU-2/Feat: Add activities functionality button

--- src/pages/Home/Components/HeroSection.tsx
+++ src/pages/Home/Components/HeroSection.tsx
@@ -2,18 +2,24 @@ interface HeroProps {
   image: string
   title: string
   description: string
-  onScrollClick: () => void
+  scrollToClubs: () => void
+  scrollToPosts: () => void
 }
 
 export const HeroSection: React.FC<HeroProps> = ({
   image,
   title,
   description,
-  onScrollClick,
+  scrollToClubs,
+  scrollToPosts,
 }) => {
   return (
-    <section className="hero py-12 relative bg-[url('/src/assets/svg.png')] bg-cover bg-center min-h-[1300px] sm:min-h-[900px] md:min-h-[700px]"    >
-      <div className="container mx-auto flex pb-48 flex-col-reverse md:flex-row items-center justify-between px-6" data-aos="fade-up" data-aos-duration="2000">
+    <section className="hero py-12 relative bg-[url('/src/assets/svg.png')] bg-cover bg-center min-h-[1300px] sm:min-h-[900px] md:min-h-[700px]">
+      <div
+        className="container mx-auto flex pb-48 flex-col-reverse md:flex-row items-center justify-between px-6"
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
         <div className="hero__container md:w-1/2 text-center md:text-left space-y-6">
           <div className="hero__info pb-4 ">
             <h1 className="md:text-5xl text-4xl font-light text-custom-text-title">
@@ -27,11 +33,14 @@ export const HeroSection: React.FC<HeroProps> = ({
           <div className="flex justify-center md:justify-start space-x-4  ">
             <button
               className=" text-white font-bold px-6 py-2 rounded-lg border-2  transform transition duration-300 hover:scale-105 hover:shadow-2xl border-custom-border  hover:bg-black hover:text-white hover:border-black"
-              onClick={onScrollClick}
+              onClick={scrollToClubs}
             >
               Clubes Afiliados
             </button>
-            <button className="border border-custom-border text-white px-6 py-2 rounded-lg shadow-md  transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:bg-black hover:text-white hover:border-black">
+            <button
+              className="border border-custom-border text-white px-6 py-2 rounded-lg shadow-md  transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:bg-black hover:text-white hover:border-black"
+              onClick={scrollToPosts}
+            >
               Actividades
             </button>
           </div>
